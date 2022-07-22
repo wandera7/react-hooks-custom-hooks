24b6a53abcccdd5eff28ad9eec6d4c9d29e358ce
wandera7 <julius.wandera@student.moringaschool.com>
2022-07-22 09:24:35 +0300

    Done

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,30 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { makeEmojiList } from "../utils";
-
+import useQuery from "../hooks/useQuery";
+import useDocumentTitle from "../hooks/useDocumentTitle";
 function ArticlePage() {
-  // fetch data for a post
   const { id } = useParams();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [post, setPost] = useState(null);
-
-  const url = `http://localhost:4000/posts/${id}`;
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch(url)
-      .then((r) => r.json())
-      .then((post) => {
-        setPost(post);
-        setIsLoaded(true);
-      });
-  }, [url]);
-
-  // set the document title
+  const { data: post, isLoaded } = useQuery(
+    `http://localhost:4000/posts/${id}`
+  );
   const pageTitle = post ? `Underreacted | ${post.title}` : "Underreacted";
-  useEffect(() => {
-    document.title = pageTitle;
-  }, [pageTitle]);
-
+  useDocumentTitle(pageTitle)
   if (!isLoaded) return <h3>Loading...</h3>;
 
   const { minutes, title, date, preview } = post;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,27 +1,11 @@
 import React, { useEffect, useState } from "react";
 import About from "./About";
 import ArticleList from "./ArticleList";
-
+import useQuery from "../hooks/useQuery";
+import useDocumentTitle from "../hooks/useDocumentTitle";
 function HomePage() {
-  // fetch data for posts
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch("http://localhost:4000/posts")
-      .then((r) => r.json())
-      .then((posts) => {
-        setPosts(posts);
-        setIsLoaded(true);
-      });
-  }, []);
-
-  // set the document title
-  useEffect(() => {
-    document.title = "Underreacted | Home";
-  }, []);
-
+  const { data: posts, isLoaded } = useQuery("http://localhost:4000/posts");
+  useDocumentTitle("Underreacted | Home")
   return (
     <>
       <About />
